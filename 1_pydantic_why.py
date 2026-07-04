10a59bce8214376139b2ae8f494b4bb840db6ecb
MuhammadTaha156 <toaha155@gmail.com>
2026-07-05 02:21:41 +0500

    finish why part

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -13,15 +13,17 @@
 # insert_patient_date("taha", 30)
 
 
-from pydantic import BaseModel
-from typing import List,Dict,Tuple
+from pydantic import BaseModel,EmailStr,AnyUrl,Field
+from typing import List,Dict,Tuple,Optional,Annotated
 
 class Patient(BaseModel):
-    name:str
-    age:int
-    weight:float
-    married:bool
-    allergies:List[str]
+    name:Annotated[str,Field(max_length=50,title="Name of PATIENT",description="Give the name of Patient in less than 50 Char",examples=["Taha","Hanzala"])]
+    email:EmailStr
+    linkedin_url:AnyUrl
+    age:int=Field(gt=0,lt=80)
+    weight:Annotated[float,Field(gt=0,strict=True)]
+    married:Annotated[bool,Field(default=None,description="Is the Patient is Married or not")]
+    allergies:Annotated[Optional[List[str]],Field(max_length=5)]
     contact_Details:Dict[str,str]
 
 
@@ -31,6 +33,9 @@ def insert_patient_data(patient:Patient):
 
     print(patient.name)
     print(patient.age)
+    print(patient.allergies)
+    print(patient.married)
+    print(patient.email)
     print("Insert into database")
 
 def update_patient_data(patient:Patient):
@@ -39,9 +44,9 @@ def update_patient_data(patient:Patient):
     print(patient.age)
     print("Insert into database")
 
-patient_info={"name":"Taha","age":"30","weight":"75.2","married":True,"allergies":["pollen","Dust"],"contact_Details":{'email':'abc@gmail.com','phone':'234556',"ID":"12"}}
+patient_info={"name":"Taha",'email':'abc@gmail.com',"linkedin_url":"https://linkedin.com/112233","age":"30","weight":75.2,"married":True,"allergies":["pollen","Dust"],"contact_Details":{'phone':'234556',"ID":"12"}}
 
 patient1=Patient(**patient_info)
 
 insert_patient_data(patient1)
-update_patient_data(patient1)
+# update_patient_data(patient1)
