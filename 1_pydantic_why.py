1e71c3e926ea21de77ea04ad79e07481fe5ffbcd
MuhammadTaha156 <toaha155@gmail.com>
2026-07-05 01:52:01 +0500

    basic

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -1,6 +1,47 @@
-def insert_patient_date(name,age):
-    print(name)
-    print(age)
+# def insert_patient_date(name: str,age: int):
+#     if type(name)==str and type(age)==int:
+#         if age<0:
+#             raise ValueError("age cant be negative")
+#         else:
+#             print(name)
+#             print(age)
+#             print("Insert into database")
+#     else:
+#         raise TypeError("Incorrect data type")
+
+
+# insert_patient_date("taha", 30)
+
+
+from pydantic import BaseModel
+from typing import List,Dict,Tuple
+
+class Patient(BaseModel):
+    name:str
+    age:int
+    weight:float
+    married:bool
+    allergies:List[str]
+    contact_Details:Dict[str,str]
+
+
+
+
+def insert_patient_data(patient:Patient):
+
+    print(patient.name)
+    print(patient.age)
     print("Insert into database")
 
-insert_patient_date("taha",12)
+def update_patient_data(patient:Patient):
+
+    print(patient.name)
+    print(patient.age)
+    print("Insert into database")
+
+patient_info={"name":"Taha","age":"30","weight":"75.2","married":True,"allergies":["pollen","Dust"],"contact_Details":{'email':'abc@gmail.com','phone':'234556',"ID":"12"}}
+
+patient1=Patient(**patient_info)
+
+insert_patient_data(patient1)
+update_patient_data(patient1)
